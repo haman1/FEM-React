2cb4ec90afed4af27c6820b48b89e2325266d17d
Ombasa Herman <ombasaherman@gmail.com>
2023-01-14 11:31:38 +0300

    App.js

---
+++ news-homepage/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import List from "./components/List";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default App;
